dcaf0deb59a9c448682faec11135cd3f96ddc8a4
koushik K M <koushik@avesorg.com>
2017-09-30 22:25:11 +0530

    Riact Changes

--- styles.js
+++ styles.js
@@ -1,9 +1,23 @@
 
 import { Platform,AppRegistry, StyleSheet} from 'react-native';
 
+var riactClr = '#60b4f3';
+var riactBtnClr = '#1485d8';
+var minWidth = 300;
+var minHeight =50;
+
 module.exports= {
 
- 
+  splashScreen :{
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex:1,
+  },
+
+  scrollViewContainer :{
+  flexGrow: 1,
+  },
+
   appHeader :
   {
     backgroundColor: (Platform.OS === 'ios') ? '#F3B059':'#F3B059',
@@ -14,9 +28,9 @@ module.exports= {
   headerText :
   {
    textAlign: 'center',
-
-    fontSize : (Platform.OS === 'ios') ? 20 : 13,
-    color: 'white',
+   marginTop:10,
+    fontSize : (Platform.OS === 'ios') ? 20 : 16,
+    color: 'black',
   },
   appBody :{
     
@@ -24,17 +38,19 @@ module.exports= {
 
   },
    primaryButton: {
-    minWidth : 270,
-    minHeight :50,
+    minWidth : minWidth,
+    minHeight :minHeight,
     borderRadius: 7,
+    marginTop:12,
+    marginBottom:12,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F79F1A',
     
   },
   roundButton : {
-     minWidth : 270,
-    minHeight :50,
+     minWidth : minWidth,
+    minHeight :minHeight,
     borderRadius: 50,
     justifyContent: 'center',
     alignItems: 'center',
@@ -45,14 +61,15 @@ module.exports= {
     color: 'white'
   },
   primaryTextInput : {
-    minWidth : 270,
-    marginBottom : 30,
+    minWidth : minWidth,
+    marginBottom : 12,
+    marginTop:12,
     minHeight :50,
     borderRadius: 7,
     padding : 5,
     justifyContent: 'center',
     alignItems: 'center',
-    borderColor: 'gray',
+    borderColor: (Platform.OS === 'ios') ? 'gray':'#fff',
     borderWidth: 1
   },
 
@@ -61,6 +78,18 @@ module.exports= {
     justifyContent: 'center',
     textAlign: 'center',
     marginBottom:30,
+  },
+
+  riactButton:{
+    minWidth : minWidth,
+    minHeight :minHeight,
+    borderRadius: 7,
+    marginTop:12,
+    marginBottom:12,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: riactBtnClr,
+
   }
 
 }
